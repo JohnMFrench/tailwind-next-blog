b4ba698137403a4c51b8caf0f9aa1678dfa47f89
jf <>
2022-05-18 07:18:51 -0400

    <Image, nav links

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,9 +1,8 @@
 const headerNavLinks = [
-  { href: '/blog', title: 'Blog' },
   { href: '/resume', title: 'Resume' },
   //{ href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
-  //{ href: '/about', title: 'About' },
+  { href: '/', title: 'About' },
 ]
 
 export default headerNavLinks
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import siteMetadata from '@/data/siteMetadata'
 import { getAllFilesFrontMatter } from '@/lib/mdx'
 import formatDate from '@/lib/utils/formatDate'
 import { GetStaticProps, InferGetStaticPropsType } from 'next'
+import Image from 'next/image';
 import { PostFrontMatter } from 'types/PostFrontMatter'
 import NewsletterForm from '@/components/NewsletterForm'
 
@@ -23,23 +24,25 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
       <div className="divide-y·divide-gray-200·bg-white">
         <div className="container mx-auto flex flex-col items-center px-5 py-16 md:flex-row">
           <div className="mb-16 flex flex-col items-center text-center md:mb-0 md:w-1/2 md:items-start md:pr-16 md:text-left lg:flex-grow lg:pr-24">
-            <h1 className="title-font mb-4 text-3xl font-medium sm:text-4xl">
+            <h1 className="title-font text-2xl font-medium mb-4 md:tracking-wide md:text-6xl">
               Hi, my name is John
               <br className="hidden lg:inline-block" />
             </h1>
             <p className="mb-8 leading-relaxed">{siteMetadata.description}</p>
             <div className="flex justify-center">
               <button className="inline-flex rounded border-0 bg-opal py-2 px-6 text-lg text-white hover:bg-opal focus:outline-none">
-                Button
+                Resume
               </button>
               <button className="ml-4 inline-flex rounded border-0 bg-gray-100 py-2 px-6 text-lg text-gray-700 hover:bg-gray-200 focus:outline-none">
-                Button
+                
               </button>
             </div>
           </div>
           <div className="w-5/6 md:w-1/2 lg:w-full lg:max-w-lg">
-            <img
+            <Image
               className=" rounded-lg object-cover object-center"
+              width="400px"
+              height="400px"
               alt="person using laptop"
               src="/static/images/e-learning/e-learning.png"
             />
