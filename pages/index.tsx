30a78a14f385f6de22eedd702baa1b7ca4a95112
jf <>
2022-05-18 07:21:56 -0400

    fixed build

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import siteMetadata from '@/data/siteMetadata'
 import { getAllFilesFrontMatter } from '@/lib/mdx'
 import formatDate from '@/lib/utils/formatDate'
 import { GetStaticProps, InferGetStaticPropsType } from 'next'
-import Image from 'next/image';
+import Image from 'next/image'
 import { PostFrontMatter } from 'types/PostFrontMatter'
 import NewsletterForm from '@/components/NewsletterForm'
 
@@ -24,7 +24,7 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
       <div className="divide-y·divide-gray-200·bg-white">
         <div className="container mx-auto flex flex-col items-center px-5 py-16 md:flex-row">
           <div className="mb-16 flex flex-col items-center text-center md:mb-0 md:w-1/2 md:items-start md:pr-16 md:text-left lg:flex-grow lg:pr-24">
-            <h1 className="title-font text-2xl font-medium mb-4 md:tracking-wide md:text-6xl">
+            <h1 className="title-font mb-4 text-2xl font-medium md:text-6xl md:tracking-wide">
               Hi, my name is John
               <br className="hidden lg:inline-block" />
             </h1>
@@ -33,9 +33,7 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
               <button className="inline-flex rounded border-0 bg-opal py-2 px-6 text-lg text-white hover:bg-opal focus:outline-none">
                 Resume
               </button>
-              <button className="ml-4 inline-flex rounded border-0 bg-gray-100 py-2 px-6 text-lg text-gray-700 hover:bg-gray-200 focus:outline-none">
-                
-              </button>
+              <button className="ml-4 inline-flex rounded border-0 bg-gray-100 py-2 px-6 text-lg text-gray-700 hover:bg-gray-200 focus:outline-none"></button>
             </div>
           </div>
           <div className="w-5/6 md:w-1/2 lg:w-full lg:max-w-lg">
